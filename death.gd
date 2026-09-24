a81fdf17647e148bfce92aef4bcbd182ff1695be
whatbeato <96067139+whatbeato@users.noreply.github.com>
2026-09-24 20:33:39 +0100

    death and win scenes and scripts, im done with ts im submitting

--- death.gd
+++ death.gd
@@ -5,4 +5,4 @@ func _ready():
 	
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("player"):
-		get_tree().chance_scene_to_file("res://death.tscn")
+		get_tree().change_scene_to_file("res://death.tscn")
--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -850.0
+const JUMP_VELOCITY = -790.0
 
 
 func _physics_process(delta: float) -> void:
