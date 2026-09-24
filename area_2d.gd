e4b3fad65d4ee9df098a02cbbcda7be49cb270e8
whatbeato <96067139+whatbeato@users.noreply.github.com>
2026-09-24 19:43:17 +0100

    coin added

---
+++ area_2d.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node2D):
+	if body.name == "Player":
+		Global.add_coin()
